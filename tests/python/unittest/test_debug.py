3abf423d9d36c0452ec23687d6b1eaa2f87c47c4
Mahyar Najibi <mahyarnajibi@gmail.com>
2018-04-20 10:11:28 -0400

    Updating the test debug

--- tests/python/unittest/test_debug.py
+++ tests/python/unittest/test_debug.py
@@ -9,18 +9,16 @@ from mxnet.test_utils import *
 from mxnet.base import py_str, MXNetError
 from common import setup_module, with_seed
 import unittest
-
+import time
 def check_concat_with_shape():
-
-
     inputs = [mx.symbol.Variable('input')]
     sym = mx.symbol.Debug(*inputs, name='debug')
-    x_nd = mx.nd.array([[1, 6, 4, 2]], ctx=mx.cpu(0))
+    x_nd = mx.nd.array([[[[1, 6, 4, 2]]]], ctx=mx.cpu(0))
     #data_npy = np.random.normal(size=(10,20,3,3))
     exe = sym.bind(ctx=mx.cpu(0), args={'input': x_nd})
+    
     outputs = exe.forward(is_train=True)
-
+    print('Done!')
 
 if __name__=='__main__':
-	import pdb;pdb.set_trace()
 	check_concat_with_shape()
