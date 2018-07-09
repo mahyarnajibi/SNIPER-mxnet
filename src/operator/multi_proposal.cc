3f0256ccf68b5040c105ee8f21fac4a4c9e25b24
Mahyar Najibi <mahyarnajibi@gmail.com>
2018-07-09 12:09:18 -0700

    Adding cpu version of proposal layer

--- src/operator/multi_proposal.cc
+++ src/operator/multi_proposal.cc
@@ -22,14 +22,24 @@
  * Licensed under The Apache-2.0 License [see LICENSE for details]
  * \file multi_proposal.cc
  * \brief Proposal target layer
- * \author Bharat Singh
 */
 
 #include "./multi_proposal-inl.h"
 #include <set>
 #include <math.h>
 #include <unistd.h>
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <mshadow/tensor.h>
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <thrust/functional.h>
+#include "./operator_common.h"
+#include "./mshadow_op.h"
 #include <time.h>
+#include <stdlib.h>
+
 //============================
 // Bounding Box Transform Utils
 //============================
@@ -38,12 +48,12 @@ namespace op {
 namespace utils {
 
 inline void BBoxTransformInv(float* boxes,
-			       const mshadow::Tensor<cpu, 4>& deltas,
-                             float* im_info) {
-  int num_images = deltas.size(0);
-  int anchors = deltas.size(1) / 4;
-  int heights = deltas.size(2);
-  int widths = deltas.size(3);
+                             float* deltas,
+                             float* im_info,
+                             int num_images,
+                             int anchors,
+                             int heights,
+                             int widths) {
   int num_anchors = anchors * heights * widths;
   //usleep(20000000);
   #pragma omp parallel for num_threads(8)
@@ -58,10 +68,10 @@ inline void BBoxTransformInv(float* boxes,
     float height = boxes[5*t + 3] - boxes[5*t + 1] + 1.0;
     float ctr_x = boxes[5*t + 0] + 0.5 * (width - 1.0);
     float ctr_y = boxes[5*t + 1] + 0.5 * (height - 1.0);
-    float dx = deltas[b][a*4 + 0][h][w];
-    float dy = deltas[b][a*4 + 1][h][w];
-    float dw = deltas[b][a*4 + 2][h][w];
-    float dh = deltas[b][a*4 + 3][h][w];
+    float dx = deltas[b*num_anchors*4 + a*4*widths*heights + h*widths + w];
+    float dy = deltas[b*num_anchors*4 + (a*4 + 1)*widths*heights + h*widths + w];
+    float dw = deltas[b*num_anchors*4 + (a*4 + 2)*widths*heights + h*widths + w];
+    float dh = deltas[b*num_anchors*4 + (a*4 + 3)*widths*heights + h*widths + w];
     float pred_ctr_x = dx * width + ctr_x;
     float pred_ctr_y = dy * height + ctr_y;
     float pred_w = exp(dw) * width;
@@ -152,10 +162,10 @@ inline void NonMaximumSuppression(float* dets,
                                   int width,
                                   int height,
                                   std::vector< std::vector<int> > & final_keep_images) {
-  
+
   int total_anchors = num_images*num_anchors*width*height;
   int chip_anchors = num_anchors*width*height;
-  
+
   float *area = new float[total_anchors];
 
   #pragma omp parallel for num_threads(8)
@@ -163,7 +173,7 @@ inline void NonMaximumSuppression(float* dets,
     area[i] = (dets[5*i + 2] - dets[5*i + 0] + 1) * (dets[5*i + 3] - dets[5*i + 1] + 1);
   }
 
-  int max_nms = 12000;
+  int max_nms = std::min(12000, chip_anchors);
   #pragma omp parallel for num_threads(8)
   for (int i = 0; i < num_images; i++) {
     std::vector <float> sortids(chip_anchors);
@@ -171,7 +181,7 @@ inline void NonMaximumSuppression(float* dets,
       sortids[j] = j;
     }
     int chip_index = i*chip_anchors;
-    std::sort(sortids.begin(), sortids.end(), 
+    std::sort(sortids.begin(), sortids.end(),
         [&dets,chip_index](int i1, int i2) {
           return dets[5*(chip_index + i1) + 4] > dets[5*(chip_index + i2) + 4];
         });
@@ -206,7 +216,7 @@ inline void NonMaximumSuppression(float* dets,
       for (int pind = j + 1; pind < max_nms; pind++) {
         if (dbuf[6*pind + 4] == -1) {
           continue;
-        } 
+        }
         float xx1 = std::max(ix1, dbuf[6*pind]);
         float yy1 = std::max(iy1, dbuf[6*pind + 1]);
         float xx2 = std::min(ix2, dbuf[6*pind + 2]);
@@ -229,10 +239,34 @@ inline void NonMaximumSuppression(float* dets,
 
 
 template<typename xpu>
-class MultiProposalOp : public Operator{
+class MultiProposalGPUOp : public Operator{
  public:
-  explicit MultiProposalOp(MultiProposalParam param) {
+ 	float *scores;
+ 	float *bbox_deltas;
+ 	float *proposals;
+ 	float *im_info;
+ 	float *rois;
+  float *out_scores;
+
+
+  explicit MultiProposalGPUOp(MultiProposalParam param) {
     this->param_ = param;
+    int batch_size = param.batch_size;
+    this->scores = new float[batch_size*21*2*200*200];
+    this->bbox_deltas = new float[batch_size*21*4*200*200];
+    this->proposals = new float[batch_size*21*5*200*200];
+    this->im_info = new float[batch_size*3];
+    this->rois = new float[param.rpn_post_nms_top_n * batch_size * 5];
+    this->out_scores = new float[param.rpn_post_nms_top_n*batch_size];
+  }
+
+  ~MultiProposalGPUOp() {
+    delete [] this->scores;
+    delete [] this->bbox_deltas;
+    delete [] this->proposals;
+    delete [] this->im_info;
+    delete [] this->rois;
+    delete [] this->out_scores;
   }
 
   virtual void Forward(const OpContext &ctx,
@@ -240,33 +274,29 @@ class MultiProposalOp : public Operator{
                        const std::vector<OpReqType> &req,
                        const std::vector<TBlob> &out_data,
                        const std::vector<TBlob> &aux_states) {
-    using namespace mshadow;
-    using namespace mshadow::expr;
     CHECK_EQ(in_data.size(), 3);
     CHECK_EQ(out_data.size(), 2);
+
+    using namespace mshadow;
+    using namespace mshadow::expr;
     //clock_t t;
-    //t = clock();
-    //std::cout << "quack 1" << std::endl;
+  	//t = clock();
     Stream<xpu> *s = ctx.get_stream<xpu>();
-    Tensor<cpu, 4> scores = in_data[proposal::kClsProb].get<cpu, 4, real_t>(s);
+    Tensor<cpu, 4> tscores = in_data[proposal::kClsProb].get<cpu, 4, real_t>(s);
     Tensor<cpu, 4> tbbox_deltas = in_data[proposal::kBBoxPred].get<cpu, 4, real_t>(s);
     Tensor<cpu, 2> tim_info = in_data[proposal::kImInfo].get<cpu, 2, real_t>(s);
-
-    Tensor<cpu, 2> rois = out_data[proposal::kRoIs].get<cpu, 2, real_t>(s);
-    Tensor<cpu, 1> out_scores = out_data[proposal::kScores].get<cpu, 1, real_t>(s);
 
     int num_images = tbbox_deltas.size(0);
     int num_anchors = tbbox_deltas.size(1) / 4;
     int height = tbbox_deltas.size(2);
     int width = tbbox_deltas.size(3);
-    //number of anchors per chip
     int count_anchors = num_anchors*height*width;
-    //std::cout << "quack 2" << std::endl;
-    //total number of anchors in a batch
     int total_anchors = count_anchors * num_images;
-    
-    float *proposals = new float[total_anchors*5];
-    float *im_info = new float[num_images*3];
+
+    memcpy(scores, tscores.dptr_, total_anchors*2*sizeof(float));
+    memcpy(bbox_deltas, tbbox_deltas.dptr_, total_anchors*4*sizeof(float));
+    memcpy(im_info, tim_info.dptr_, 3 * sizeof(float) * num_images);
+
 
     std::vector<float> base_anchor(4);
     //usleep(20000000);
@@ -294,18 +324,10 @@ class MultiProposalOp : public Operator{
       proposals[5*t + 1] = anchors[4*i+1] + j * param_.feature_stride;
       proposals[5*t + 2] = anchors[4*i+2] + k * param_.feature_stride;
       proposals[5*t + 3] = anchors[4*i+3] + j * param_.feature_stride;
-      proposals[5*t + 4] = scores[b][1][i*height + j][k];
+      proposals[5*t + 4] = scores[b*count_anchors*2 + ((num_anchors + i)*height + j)*width + k];
     }
 
-    //std::cout << "quack 4" << std::endl;
-    //copy im_info
-    for (int i = 0; i < num_images; i++) {
-      im_info[i*3] = tim_info[i][0];
-      im_info[i*3+1] = tim_info[i][1];
-      im_info[i*3+2] = tim_info[i][2];
-    }
-
-    utils::BBoxTransformInv(proposals, tbbox_deltas, im_info);
+    utils::BBoxTransformInv(proposals, bbox_deltas, im_info, num_images, num_anchors, height, width);
 
     utils::FilterBox(proposals, total_anchors, 3);
 
@@ -322,28 +344,32 @@ class MultiProposalOp : public Operator{
       int numpropsi = keep_images[i].size();
       for (int j = 0; j < numpropsi; j++) {
         int base = (i*rpn_post_nms_top_n + j);
-        rois[base][0] = i;
-        rois[base][1] = proposals[5*keep_images[i][j] + 0];
-        rois[base][2] = proposals[5*keep_images[i][j] + 1];
-        rois[base][3] = proposals[5*keep_images[i][j] + 2];
-        rois[base][4] = proposals[5*keep_images[i][j] + 3];
-        out_scores[base] = proposals[keep_images[i][j] + 4];
+        rois[5*base] = i;
+        rois[5*base+1] = proposals[5*keep_images[i][j] + 0];
+        rois[5*base+2] = proposals[5*keep_images[i][j] + 1];
+        rois[5*base+3] = proposals[5*keep_images[i][j] + 2];
+        rois[5*base+4] = proposals[5*keep_images[i][j] + 3];
+        out_scores[base] = proposals[5*keep_images[i][j] + 4];
       }
 
       for (int j = numpropsi; j < rpn_post_nms_top_n; j++) {
         int base = (i*rpn_post_nms_top_n + j);
-        rois[base][0] = i;
-        rois[base][1] = 0;
-        rois[base][2] = 0;
-        rois[base][3] = 100;
-        rois[base][4] = 100;
-        out_scores[base] = 0;
+        rois[5*base+0] = i;
+        rois[5*base+1] = rand() % 100;
+        rois[5*base+2] = rand() % 100;
+        rois[5*base+3] = 200 + rand() % 200;
+        rois[5*base+4] = 200 + rand() % 200;
+        out_scores[base] = 0.0;
       }
 
     }
 
-    delete [] im_info;
-    delete [] proposals;
+    Stream<xpu> *so = ctx.get_stream<xpu>();
+    Tensor<cpu,1> oscores = out_data[proposal::kScores].get<cpu, 1, real_t>(so);
+    Tensor<cpu, 2> orois = out_data[proposal::kRoIs].get<cpu, 2, real_t>(so);
+    memcpy(orois.dptr_, rois, 5*sizeof(float) * num_images * rpn_post_nms_top_n);
+    memcpy(oscores.dptr_, out_scores, sizeof(float) * num_images * rpn_post_nms_top_n);
+
   }
 
   virtual void Backward(const OpContext &ctx,
@@ -374,9 +400,8 @@ class MultiProposalOp : public Operator{
 
 template<>
 Operator *CreateOp<cpu>(MultiProposalParam param) {
-  return new MultiProposalOp<cpu>(param);
+  return new MultiProposalGPUOp<cpu>(param);
 }
-
 Operator* MultiProposalProp::CreateOperator(Context ctx) const {
   DO_BIND_DISPATCH(CreateOp, param_);
 }
@@ -384,10 +409,10 @@ Operator* MultiProposalProp::CreateOperator(Context ctx) const {
 DMLC_REGISTER_PARAMETER(MultiProposalParam);
 
 MXNET_REGISTER_OP_PROPERTY(MultiProposal, MultiProposalProp)
-.describe("Generate region proposals via RPN")
-.add_argument("cls_prob", "NDArray-or-Symbol", "Score of how likely proposal is object.")
-.add_argument("bbox_pred", "NDArray-or-Symbol", "BBox Predicted deltas from anchors for proposals")
-.add_argument("im_info", "NDArray-or-Symbol", "Image size and scale.")
+.describe("Proposal Generator")
+.add_argument("cls_prob", "NDArray-or-Symbol", "Proposal Score")
+.add_argument("bbox_pred", "NDArray-or-Symbol", "Bounding Box Prediction")
+.add_argument("im_info", "NDArray-or-Symbol", "Image Info")
 .add_arguments(MultiProposalParam::__FIELDS__());
 
 }  // namespace op
